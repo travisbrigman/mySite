f94fa23c04b9dc9c997f60a1436c3d6ba87a011a
travisbrigman <travis1000@gmail.com>
2022-05-05 21:46:53 -0500

    fixes styling. adds images for uxcoe page.

--- Sources/App/Controllers/MainController.swift
+++ Sources/App/Controllers/MainController.swift
@@ -20,7 +20,7 @@ struct MainController: RouteCollection {
             Card(
                 title: "Personal Site",
                 description: "The page you are viewing now is built with Vapor and Bulma",
-                imagePath: nil,
+                imagePath: "/images/wave.svg",
                 icon: "fa-solid fa-droplet",
                 linkRoute: "personal-site"
                 
@@ -28,7 +28,7 @@ struct MainController: RouteCollection {
             Card(
                 title: "HCA Healthcare's UXCoE",
                 description: "The User Experience Center of Excellence wanted a custom page not hosted on a rigid CMS.",
-                imagePath: nil,
+                imagePath: "/images/portfolio/UXCOE.png",
                 icon: "fa-brands fa-react",
                 linkRoute: "uxcoe"
                 
@@ -57,7 +57,7 @@ struct MainController: RouteCollection {
             DetailContext(name: "seat-geek", title: "Seat Geek", description: "This is a code challenge I did for a company called FetchNow. It consumes a list of events from the SeatGeek API and displays them in a list. A user can tap a list item and see a detail view of that item. The user can like the event in the detail view and see liked events back in the table view. The likes persist even if the app is closed.", imageURL: "/images/portfolio/SeatGeek-1.png", liveSite: nil, gitHubRepo:"https://github.com/travisbrigman/FetchNow"),
             DetailContext(name: "rare", title: "Rare", description: "Rare started off as class project in the bootcamp I attended. We built the frontend using React and the backend with Django. I did a lot of the styling for the frontend and I was pleased with how it turned out so I decided to rebuild the backend in Vapor after I completed the bootcamp. This was my first Vapor project and I learned a lot! Switching from one framework to another helped reinforce foundational concepts like how databases interact with ORM’s and creating routes. I also learned that switching backends on a site doesn’t mean “you just change some endpoints and you’re done.” I did a lot of refactoring of the React front end to make it work well with the new backend.", imageURL: "/images/portfolio/rare.png", liveSite:"https://rare-client.herokuapp.com/login", gitHubRepo:"https://github.com/travisbrigman/rare-server-vapor"),
             DetailContext(name: "personal-site", title: "My Personal Site", description: "The site you are on now is built with the Swift server side framework Vapor. Each page is a template being rendered as a URL route is called. Even though Vapor is a server side framework, the minimal amount of content being presented is being store with the framework code. Styling is done with the Bulma CSS framework. The reasons behind using Bulma are mostly because it looks great out of the box and I wanted to try something new and different", imageURL: "/images/portfolio/rare.jpg", liveSite: nil, gitHubRepo:"https://github.com/travisbrigman/mySite"),
-            DetailContext(name: "uxcoe", title: "HCA Healthcare's User Experience Center of Excellence", description: "This was a project I worked on for the first department I worked in at HCA Healthcare. Like any big enterprise, HCA uses a CMS that allows each department to build a site. It's not that great at creating beautiful, well designed sites. They asked me to build a frontend using React and the in house design system Neutron. The Neutron design system is converted into code using Ionic’s Stencil system. From there, a developer can use any of the most popular Javascript frameworks. I learned a lot about the development process from this site, but I think the biggest takeaway is the importance of design. It’s hard enough coding a site, so it was a relief that I didn’t have to come up with layouts, color palettes, illustrations, etc.", imageURL: "/images/portfolio/rare.jpg", liveSite: nil, gitHubRepo: nil)
+            DetailContext(name: "uxcoe", title: "HCA Healthcare's User Experience Center of Excellence", description: "This was a project I worked on for the first department I worked in at HCA Healthcare. Like any big enterprise, HCA uses a CMS that allows each department to build a site. It's not that great at creating beautiful, well designed sites. They asked me to build a frontend using React and the in house design system Neutron. The Neutron design system is converted into code using Ionic’s Stencil system. From there, a developer can use any of the most popular Javascript frameworks. I learned a lot about the development process from this site, but I think the biggest takeaway is the importance of design. It’s hard enough coding a site, so it was a relief that I didn’t have to come up with layouts, color palettes, illustrations, etc.", imageURL: "/images/portfolio/UXCOE2.png", liveSite: "https://uxcoe.hcahealthcare.cloud", gitHubRepo: nil)
 
 
         ]
